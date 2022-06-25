afa29602638535854da7d221181d86a2e3ab683b
ALQAISI yasir <yasir.alqaisi.ext@up.coop>
2022-06-25 15:28:08 +0200

    change url

--- src/comp/api.js
+++ src/comp/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 import env from '../env';
 const api = {
-    spendings:() => axios.get(env()+"spendings"),
-    monthlySpending:() => axios.get(env()+"spendings/monthly"),
-    dailySpending:() => axios.get(env()+"spendings/daily"),
-    yearlySpending:() => axios.get(env()+"spendings/yearly"),
+    spendings:() => axios.get(env()+"spand/spendings"),
+    monthlySpending:() => axios.get(env()+"spand/monthly"),
+    dailySpending:() => axios.get(env()+"spand/daily"),
+    yearlySpending:() => axios.get(env()+"spand/yearly"),
 
 
 
-    addSpending:(spending) => axios.put(env()+"spendings", spending)
+    addSpending:(spending) => axios.put(env()+"spand/spendings", spending)
 }
 
 export default api;
