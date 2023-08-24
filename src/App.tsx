42d29704a8ee7ffb5028ac28ed08d9f70489e480
ALQAISI yasir <yasir.alqaisi.ext@up.coop>
2023-08-24 14:38:52 +0200

    upadtes

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,6 @@ import React from 'react';
 import List from './comp/List';
 import axios from 'axios'
 
-axios.interceptors.request.use(
-    function(config) {
-      // Do something before request is sent
-      config.withCredentials = true;
-      return config;
-    },
-    function(error) {
-      // Do something with request error
-      return Promise.reject(error);
-    }
-  );
-
-  axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-  axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
 
 function App() {
   return (
