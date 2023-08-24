88779ec84eb4a09efa6f61db5cd30ac60dd41079
ALQAISI yasir <yasir.alqaisi.ext@up.coop>
2023-08-24 13:08:54 +0200

    add env

--- src/env.js
+++ src/env.js
@@ -2,7 +2,7 @@ const env = () => {
   if ( process.env.NODE_ENV === 'development' ) {
     return 'https://dev-api.yaz-fr.com/';
   }else {
-    return 'https://api.yaz-fr.com/';
+    return 'http://yasiralqaisi.great-site.net/';
   }
   return '';
 };
