2e73cf40bcfd1e9c677c6e0459363c58c0c5c88c
ALQAISI yasir <yasir.alqaisi.ext@up.coop>
2023-08-24 14:33:33 +0200

    config axios

--- src/comp/api.js
+++ src/comp/api.js
@@ -1,20 +1,3 @@
-import axios from 'axios'
-import env from '../env';
-
-axios.interceptors.request.use(
-    function(config) {
-      // Do something before request is sent
-      config.withCredentials = true;
-      return config;
-    },
-    function(error) {
-      // Do something with request error
-      return Promise.reject(error);
-    }
-  );
-
-  axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-  axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
 
 const api = {
     spendings:() => axios.get(env()+"spend/spendings"),
