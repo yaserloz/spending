43faf27efad9c15ba21f92558135935ec7e75c49
ALQAISI yasir <yasir.alqaisi.ext@up.coop>
2023-08-24 14:00:12 +0200

    add secure

--- src/env.js
+++ src/env.js
@@ -2,7 +2,7 @@ const env = () => {
   if ( process.env.NODE_ENV === 'development' ) {
     return 'https://dev-api.yaz-fr.com/';
   }else {
-    return 'http://yasiralqaisi.great-site.net/';
+    return 'https://yasiralqaisi.great-site.net/';
   }
   return '';
 };
